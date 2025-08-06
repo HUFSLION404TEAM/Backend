8d721c2c9b3c0701084516c587d6982f4088479f
HyunWoo9930 <ohw9930@hufs.ac.kr>
2025-08-06 17:50:38 +0900

    Feat : 카카오, 네이버 로그인 추가

--- src/main/java/hufs/lion/team404/oauth/OAuthAttributes.java
+++ src/main/java/hufs/lion/team404/oauth/OAuthAttributes.java
@@ -17,7 +17,8 @@ public class OAuthAttributes {
 	private String providerId;
 
 	@Builder
-	public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture, String provider, String providerId) {
+	public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email,
+		String picture, String provider, String providerId) {
 		this.attributes = attributes;
 		this.nameAttributeKey = nameAttributeKey;
 		this.name = name;
@@ -27,18 +28,59 @@ public class OAuthAttributes {
 		this.providerId = providerId;
 	}
 
-	public  static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+	public static OAuthAttributes of(String registrationId, String userNameAttributeName,
+		Map<String, Object> attributes) {
+		System.out.println("sda");
+		if ("naver".equals(registrationId)) {
+			return ofNaver(registrationId, userNameAttributeName, attributes);
+		} else if ("kakao".equals(registrationId)) {
+			return ofKakao(registrationId, userNameAttributeName, attributes);
+		}
+
 		return ofGoogle(registrationId, userNameAttributeName, attributes);
 	}
 
-	private static OAuthAttributes ofGoogle(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+	private static OAuthAttributes ofGoogle(String registrationId, String userNameAttributeName,
+		Map<String, Object> attributes) {
 		return OAuthAttributes.builder()
-			.name((String) attributes.get("name"))
-			.email((String) attributes.get("email"))
-			.picture((String) attributes.get("picture"))
+			.name((String)attributes.get("name"))
+			.email((String)attributes.get("email"))
+			.picture((String)attributes.get("picture"))
 			.attributes(attributes)
 			.provider(registrationId)
-			.providerId((String) attributes.get("sub"))
+			.providerId((String)attributes.get("sub"))
+			.nameAttributeKey(userNameAttributeName)
+			.build();
+	}
+
+	private static OAuthAttributes ofNaver(String registrationId, String userNameAttributeName,
+		Map<String, Object> attributes) {
+		Map<String, Object> response = (Map<String, Object>)attributes.get("response");
+
+		return OAuthAttributes.builder()
+			.name((String)response.get("name"))
+			.email((String)response.get("email"))
+			.picture((String)response.get("profile_image"))
+			.attributes(attributes)
+			.provider(registrationId)
+			.providerId((String)response.get("id"))
+			.nameAttributeKey(userNameAttributeName)
+			.build();
+	}
+
+	private static OAuthAttributes ofKakao(String registrationId, String userNameAttributeName,
+		Map<String, Object> attributes) {
+		System.out.println("attributes = " + attributes);
+		Map<String, Object> kakaoAccount = (Map<String, Object>)attributes.get("kakao_account");
+		Map<String, Object> profile = (Map<String, Object>)kakaoAccount.get("profile");
+
+		return OAuthAttributes.builder()
+			.name((String)profile.get("nickname"))
+			.email((String)kakaoAccount.get("email"))
+			.picture((String)profile.get("profile_image_url"))
+			.attributes(attributes)
+			.provider(registrationId)
+			.providerId(String.valueOf(attributes.get("id")))
 			.nameAttributeKey(userNameAttributeName)
 			.build();
 	}
