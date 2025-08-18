651216a4d2e7f0ff146b036b9616e377d044818f
Yebin0319 <kuyebin@naver.com>
2025-08-18 15:57:03 +0900

    Refactor: fix pull request

--- src/main/java/hufs/lion/team404/controller/RecruitingController.java
+++ src/main/java/hufs/lion/team404/controller/RecruitingController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import hufs.lion.team404.domain.dto.response.RecruitingResponse;
 import hufs.lion.team404.domain.entity.Recruiting;
 import hufs.lion.team404.domain.entity.RecruitingImage;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +29,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/recruit")
@@ -73,7 +75,12 @@ public class RecruitingController {
 	)
 	public ApiResponse<RecruitingResponse> getRecruitingById(@PathVariable Long recruitingId) {
 
-		Recruiting recruiting = recruitingModel.getRecruitingById(recruitingId);
+		Recruiting recruiting;
+		try {
+			recruiting = recruitingModel.getRecruitingById(recruitingId);
+		} catch (IllegalArgumentException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 구인글을 찾을 수 없습니다.");
+		}
 
 		RecruitingResponse dto = RecruitingResponse.builder()
 				.id(recruiting.getId())
