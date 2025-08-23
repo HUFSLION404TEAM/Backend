3d2bf3bcc99d7be1f8cad1864eabf527a3cceff8
HyunWoo9930 <ohw9930@hufs.ac.kr>
2025-08-23 23:02:19 +0900

    Feat : CrossOrigin 추가

--- src/main/java/hufs/lion/team404/controller/HomeController.java
+++ src/main/java/hufs/lion/team404/controller/HomeController.java
@@ -3,11 +3,14 @@ package hufs.lion.team404.controller;
 import hufs.lion.team404.domain.dto.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Tag(name = "시스템", description = "시스템 상태 확인 API")
+@CrossOrigin(origins = "*")
 public class HomeController {
     
     @GetMapping("/health")
